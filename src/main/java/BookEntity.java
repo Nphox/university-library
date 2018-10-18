c9afbdb177de94352305b3cb7f36f4dcc806b79c
Dmitry <nphoxconnect@gmail.com>
2018-10-18 10:15:59 +0300

    BookEntity was added

---
+++ src/main/java/BookEntity.java
@@ -0,0 +1,29 @@
+public class BookEntity {
+    private int id;
+    private String bookName;
+    private String publicationYear;
+    private String publisher;
+
+    public BookEntity(int id, String bookName, String publicationYear, String publisher) {
+        this.id = id;
+        this.bookName = bookName;
+        this.publicationYear = publicationYear;
+        this.publisher = publisher;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public String getPublicationYear() {
+        return publicationYear;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+}
